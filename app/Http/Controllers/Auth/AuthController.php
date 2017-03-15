c8de0c11cf42bcbb72a5b7cfcfbca5d2cae8af4b
raingrave <raintech@outlook.com>
2017-03-15 01:05:34 -0300

    Update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,7 +10,7 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
-    public function authorize(Request $request)
+    public function authenticate(Request $request)
     {
     	$credentials = $request->only('email', 'password');
     	try {
